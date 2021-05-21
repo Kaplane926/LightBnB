3f7c4413f3fa69a5bd8f90b66785c490692b411d
Eli Kaplan <elikaplan@me.com>
2021-05-21 18:39:40 +0000

    update the DB when the form is submittted

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -186,13 +186,7 @@ const getAllProperties = function(options, limit = 10) {
   })
   .catch(err=> console.log(err));
 }
-/*const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-  .then(res => res.rows);
-}*/
+
 exports.getAllProperties = getAllProperties;
 
 
@@ -201,10 +195,26 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+/*const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
+}*/
+
+const addProperty = function(property) {
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, conver _photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `
+    pool.query(queryString, values)
+    .then(res=> {
+      return res.rows
+    })
+    .catch(err=>{
+      console.log(err)
+    })
 }
 exports.addProperty = addProperty;
